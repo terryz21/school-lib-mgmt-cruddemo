40c4affff6fd83fe0b610afe75cb1ab114c4fbcf
Terry <terrytomzhang@gmail.com>
2020-06-16 23:52:57 -0700

    Created entities to represent book checkout/return request

--- src/main/java/com/demo/libmgmt/cruddemo/entity/CheckOutRequest.java
+++ src/main/java/com/demo/libmgmt/cruddemo/entity/CheckOutRequest.java
@@ -1,5 +1,51 @@
 package com.demo.libmgmt.cruddemo.entity;
 
-public class CheckOutRequest {
+import java.util.Date;
 
+public class CheckOutRequest {
+    private String requestId;
+    private Date checkOutDate;
+    private Date maxReturnDate;
+    private Long sid;
+
+    public CheckOutRequest() {}
+
+    public CheckOutRequest(String requestId, Date checkOutDate, Date maxReturnDate, Long sid) {
+        this.requestId = requestId;
+        this.checkOutDate = checkOutDate;
+        this.maxReturnDate = maxReturnDate;
+        this.sid = sid;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Date getMaxReturnDate() {
+        return maxReturnDate;
+    }
+
+    public void setMaxReturnDate(Date maxReturnDate) {
+        this.maxReturnDate = maxReturnDate;
+    }
+
+    public Long getSid() {
+        return sid;
+    }
+
+    public void setSid(Long sid) {
+        this.sid = sid;
+    }
 }
